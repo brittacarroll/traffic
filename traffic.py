bbadd079cb2393be3f61e03e0a50e5624819e9ed
Britta Carroll <brittacarroll@gmail.com>
2024-12-19 23:15:44 -0500

    modified model

--- traffic.py
+++ traffic.py
@@ -40,7 +40,6 @@ def main():
     # Evaluate neural network performance
     model.evaluate(x_test,  y_test, verbose=2)
 
-    pdb.set_trace()
     # Save model to file
     if len(sys.argv) == 3:
         filename = sys.argv[2]
@@ -97,22 +96,28 @@ def get_model():
 
         # Convolutional layer. Learn 32 filters using a 3x3 kernel
         tf.keras.layers.Conv2D(
-            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+            64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
         ),
 
         # Max-pooling layer, using 2x2 pool size
         tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
+        tf.keras.layers.Conv2D(
+            64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+ 
         # Flatten units
         tf.keras.layers.Flatten(),
 
         # Add a hidden layer with dropout
-        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dense(200, activation="relu"),
+
         tf.keras.layers.Dropout(0.5),
 
         # Add an output layer with output units for all 10 digits
         tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
-    ])
+    ])    
 
     # Train neural network
     model.compile(
